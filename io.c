9de844fa6db88360e4f4ef03905e380012c5c4e1
John Thomas <thelimitinfinity@hotmail.com>
2020-04-27 20:07:07 -0700

    Added rx and tx to the uart test file.
    This add basic read and write address commands over uart.

--- io.c
+++ io.c
@@ -1,25 +1,154 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2015 Chuck McManis <cmcmanis@mcmanis.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <libopencm3/stm32/usart.h>
 
+/*
+ * This is a pretty classic ring buffer for characters
+ */
+#define USART UART4_BASE
+
+#define BUFLEN 127
+static uint16_t start_ndx;
+static uint16_t end_ndx;
+static char buf[BUFLEN+1];
+#define buf_len ((end_ndx - start_ndx) % BUFLEN)
+static inline int inc_ndx(int n) { return ((n + 1) % BUFLEN); }
+static inline int dec_ndx(int n) { return (((n + BUFLEN) - 1) % BUFLEN); }
+
+int isspace(int c) {
+    switch (c) {
+        case ' ':
+        case '\n':
+        case '\t':
+        case '\v':
+        case '\f':
+        case '\r':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+/* back up the cursor one space */
+static inline void back_up(void) {
+	end_ndx = dec_ndx(end_ndx);
+	usart_send_blocking(USART, '\010');
+	usart_send_blocking(USART, ' ');
+	usart_send_blocking(USART, '\010');
+}
+
+/*
+ * A buffered line editing function.
+ */
+void get_buffered_line(void) {
+	char	c;
+
+	if (start_ndx != end_ndx) {
+		return;
+	}
+	while (1) {
+		c = usart_recv_blocking(USART);
+		if (c == '\r') {
+			buf[end_ndx] = '\n';
+			end_ndx = inc_ndx(end_ndx);
+			buf[end_ndx] = '\0';
+			usart_send_blocking(USART, '\r');
+			usart_send_blocking(USART, '\n');
+			return;
+		}
+		/* ^H or DEL erase a character */
+		if ((c == '\010') || (c == '\177')) {
+			if (buf_len == 0) {
+				usart_send_blocking(USART, '\a');
+			} else {
+				back_up();
+			}
+		/* ^W erases a word */
+		} else if (c == 0x17) {
+			while ((buf_len > 0) &&
+					(!(isspace((int) buf[end_ndx])))) {
+				back_up();
+			}
+		/* ^U erases the line */
+		} else if (c == 0x15) {
+			while (buf_len > 0) {
+				back_up();
+			}
+		/* Non-editing character so insert it */
+		} else {
+			if (buf_len == (BUFLEN - 1)) {
+				usart_send_blocking(USART, '\a');
+			} else {
+				buf[end_ndx] = c;
+				end_ndx = inc_ndx(end_ndx);
+				usart_send_blocking(USART, c);
+			}
+		}
+	}
+}
+
+/*
+ * Called by libc stdio fwrite functions
+ */
 int _write(int fd, char *ptr, int len) {
-    int i = 0;
+	int i = 0;
 
-    /*
-     * Write "len" of char from "ptr" to file id "fd"
-     * Return number of char written.
-     *
-     * Only work for STDOUT, STDIN, and STDERR
-     */
-    if (fd > 2) {
-        return -1;
-    }
+	/*
+	 * Write "len" of char from "ptr" to file id "fd"
+	 * Return number of char written.
+	 *
+	 * Only work for STDOUT, STDIN, and STDERR
+	 */
+	if (fd > 2) {
+		return -1;
+	}
+	while (*ptr && (i < len)) {
+		usart_send_blocking(USART, *ptr);
+		if (*ptr == '\n') {
+			usart_send_blocking(USART, '\r');
+		}
+		i++;
+		ptr++;
+	}
+	return i;
+}
 
-    while (*ptr && (i < len)) {
-        usart_send_blocking(UART4_BASE, *ptr);
-        if (*ptr == '\n') {
-            usart_send_blocking(UART4_BASE, '\r');
-        }
-        i++;
-        ptr++;
-    }
-    return i;
+/*
+ * Called by the libc stdio fread fucntions
+ *
+ * Implements a buffered read with line editing.
+ */
+int _read(int fd, char *ptr, int len) {
+	int	my_len;
+
+	if (fd > 2) {
+		return -1;
+	}
+
+	get_buffered_line();
+	my_len = 0;
+	while ((buf_len > 0) && (len > 0)) {
+		*ptr++ = buf[start_ndx];
+		start_ndx = inc_ndx(start_ndx);
+		my_len++;
+		len--;
+	}
+	return my_len; /* return the length we got */
 }
--- uart_test.cpp
+++ uart_test.cpp
@@ -1,11 +1,10 @@
-#include <cstring>
 #include <cstdio>
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 
-static void setup_clock(void) {
+static void clock_setup(void) {
     // HSI16 at 16Mhz
     rcc_set_hpre(RCC_CFGR_HPRE_DIV2); // Set to 16Mhz/2 = 8Mhz
     rcc_set_ppre1(RCC_CFGR_PPRE1_DIV2); // Set to 8Mhz/2 = 4Mhz
@@ -20,63 +19,57 @@ static void setup_clock(void) {
     rcc_apb2_frequency = 4000000;
 }
 
-void setup_gpio() {
-    // PC9  : BACKLIGHT ON/OFF
-    // PC10 : UART TX
-    // PC11 : UART TX
+static void gpio_setup(void) {
+    int UART4_PINS = GPIO10 | GPIO11;
     rcc_periph_clock_enable(RCC_GPIOC);
-    rcc_periph_clock_enable(RCC_UART4);
-    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
-    gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10 | GPIO11);
-    gpio_set_af(GPIOC, GPIO_AF8 /* UART_4 */, GPIO10 | GPIO11);
+    gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, UART4_PINS);
+    gpio_set_af(GPIOC, GPIO_AF8 /* UART_4 */, UART4_PINS);
 }
 
-void setup_uart() {
+void uart_setup () {
+    rcc_periph_clock_enable(RCC_UART4);
     usart_set_baudrate(UART4_BASE, 9600);
     usart_set_databits(UART4_BASE, 8);
     usart_set_stopbits(UART4_BASE, USART_STOPBITS_1);
-    usart_set_mode(UART4_BASE, USART_MODE_TX);
+    usart_set_mode(UART4_BASE, USART_MODE_TX_RX);
     usart_set_parity(UART4_BASE, USART_PARITY_NONE);
     usart_set_flow_control(UART4_BASE, USART_FLOWCONTROL_NONE);
     usart_enable(UART4_BASE);
 }
 
-int _write(int fd, char *ptr, int len) {
-    int i = 0;
-
-    /*
-     * Write "len" of char from "ptr" to file id "fd"
-     * Return number of char written.
-     *
-     * Only work for STDOUT, STDIN, and STDERR
-     */
-    if (fd > 2) {
-        return -1;
-    }
-
-    while (*ptr && (i < len)) {
-        usart_send_blocking(UART4_BASE, *ptr);
-        if (*ptr == '\n') {
-            usart_send_blocking(UART4_BASE, '\r');
-        }
-        i++;
-        ptr++;
-    }
-    return i;
-}
+const char *bit_rep[16] = {
+    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
+    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
+    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
+    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
+};
 
 int main(void) {
+    clock_setup();
+    gpio_setup();
+    uart_setup();
 
-    setup_clock();
-    setup_gpio();
-    setup_uart();
+    printf("Welcome to Bip\n");
 
-    while (true) {
-        printf("Hello World!\n");
-        for (int i = 0; i < 2500000; i++) __asm__("nop");	/* Wait a bit. */
+    int* address;
+    int value;
+
+    while(true) {
+        char c;
+        if (scanf("%c", &c)) {
+            switch(c) {
+                case 'r':
+                    scanf(" %x", &address);
+                    printf("0x%x\n", *address);
+                    break;
+                case 'w':
+                    scanf(" %x %x", &address, &value);
+                    *address = value;
+                    printf("0x%x = 0x%x\n", address, *address);
+                    break;
+            }
+        }
     }
 
     return 0;
 }
-
-
