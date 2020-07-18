e3f29dcefb7fd55cd3a1699f34225a38d716600f
John Thomas <thelimitinfinity@hotmail.com>
2020-07-18 16:50:11 -0700

    Completed LCD Demo. Demo scrolls all 8 colors down the display.

--- lcd_test.cpp
+++ lcd_test.cpp
@@ -1,47 +1,28 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/pwr.h>
 
-//#include "lcd_lpm013m126c.hpp"
-
-static void clock_setup(void) {
-    // HSI16 at 16Mhz
-    rcc_set_hpre(RCC_CFGR_HPRE_DIV2); // Set to 16Mhz/2 = 8Mhz
-    rcc_set_ppre1(RCC_CFGR_PPRE1_DIV2); // Set to 8Mhz/2 = 4Mhz
-    rcc_set_ppre2(RCC_CFGR_PPRE2_DIV2); // Set to 8Mhz/2 = 4Mhz
-
-    rcc_osc_on(RCC_HSI16);
-    rcc_wait_for_osc_ready(RCC_HSI16);
-    rcc_set_sysclk_source(RCC_CFGR_SW_HSI16);
-
-    rcc_ahb_frequency  = 8000000;
-    rcc_apb1_frequency = 4000000;
-    rcc_apb2_frequency = 4000000;
-}
+#define LCD_CS            GPIOG, GPIO12
+#define LCD_SPI           SPI3
 
 static void gpio_setup(void) {
+    // PC7 : 2.8v ON/OFF
+    // PC8 : VIBRO PWM
+    // PC9 : BACKLIGHT ON/OFF
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7 | GPIO8 | GPIO9);
+    gpio_clear(GPIOC, GPIO8 | GPIO9);
+    gpio_set(GPIOC, GPIO7); // Enable power for the backlight, screen and vibro. Screen will fade to black if this is disabled.
 
-    // Enable the main button.
-	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO0);
+    // Since the GPIOG port is powered by VDDIO2 on L4 chips, we need to specifically
+    // enable power for this port by setting the IOSV bit in PWR_CR2.
+    rcc_periph_clock_enable(RCC_PWR);
+    uint32_t pwr_reg32 = PWR_CR2;
+    pwr_reg32 |= PWR_CR2_IOSV;
+    PWR_CR2 = pwr_reg32;
 
-	/* Enable GPIOB clock. */
-	rcc_periph_clock_enable(RCC_GPIOB);
-    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
-    gpio_set(GPIOB, GPIO12);
-
-    // PC7 : PWM 2.8v ON/OFF
-    // PC8 : ??
-    // PC9 : BACK LIGHT ON/OFF
-	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7 | GPIO8 | GPIO9);
-    gpio_set(GPIOC, GPIO7);
-    gpio_clear(GPIOC, GPIO8);
-}
-
-static void spi_setup(void) {
-	rcc_periph_clock_enable(RCC_GPIOG);
-	rcc_periph_clock_enable(RCC_SPI3);
+    rcc_periph_clock_enable(RCC_GPIOG);
 
     // PG9  : LCD_1 - SCLK
     // PG10 : MISO (Not used)
@@ -49,79 +30,83 @@ static void spi_setup(void) {
     // PG12 : LCD_3 - SCS
     // PG13 : LCD_5 - ON/OFF
     // PG14 : LCD_4 - EXTCOM IN
+    uint16_t SPI3_PINS = GPIO9 | GPIO11;
+    gpio_mode_setup(GPIOG, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, SPI3_PINS);
+    gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12 | GPIO13 | GPIO14);
+	gpio_set_af(GPIOG, GPIO_AF6, SPI3_PINS);
 
     // Turn the display on.
-    gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13 | GPIO14);
     gpio_set(GPIOG, GPIO13);
-    gpio_clear(GPIOG, GPIO14);
 
-    uint16_t G_SPI_PINS = GPIO9 | GPIO10 | GPIO11 | GPIO12;
-    gpio_mode_setup(GPIOG, GPIO_MODE_AF, GPIO_PUPD_NONE, G_SPI_PINS);
-	gpio_set_af(GPIOG, GPIO_AF6, G_SPI_PINS);
+    // Clear Chip select and polarity bit.
+    gpio_clear(GPIOG, GPIO12 | GPIO14);
+}
 
-    uint16_t C_SPI_PINS = GPIO10 | GPIO11 | GPIO12;
-    gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, C_SPI_PINS);
-	gpio_set_af(GPIOC, GPIO_AF6, C_SPI_PINS);
-
-
+static void spi_setup() {
+    rcc_periph_clock_enable(RCC_SPI3);
 	// Set the baudrate to 4Mhz APB1 / 2 = 2Mhz
-    spi_reset(SPI3);
-	spi_init_master(SPI3, SPI_CR1_BAUDRATE_FPCLK_DIV_2, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_MSBFIRST);
+    spi_init_master(LCD_SPI, SPI_CR1_BAUDRATE_FPCLK_DIV_2, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+                   SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_MSBFIRST);
+    spi_disable_software_slave_management(LCD_SPI);
+    spi_set_bidirectional_transmit_only_mode(LCD_SPI);
+    spi_set_data_size(LCD_SPI, SPI_CR2_DS_8BIT);
+	spi_enable_ss_output(LCD_SPI);
+    spi_enable(LCD_SPI);
+}
 
-    spi_disable_software_slave_management(SPI3);
-	spi_enable_ss_output(SPI3); /* Required, see NSS, 25.3.1 section. */
+void send_line(uint8_t line, uint8_t data) {
+    // Enable CS
+    gpio_set(LCD_CS);
 
-    spi_enable(SPI3);
+    spi_send8(LCD_SPI, 0x90); // 4-bit data mode
+    spi_send8(LCD_SPI, line+1);
+
+    // 4-bit * 176px / 8-bit
+    for(int x = 0 ; x < 176 * 4 / 8; ++x){
+        spi_send8(LCD_SPI, data);
+    }
+
+    // 2 dummy bytes to complete transfer
+    spi_send8(LCD_SPI, 0x0);
+    spi_send8(LCD_SPI, 0x0);
+    while (SPI_SR(LCD_SPI) & SPI_SR_BSY);
+
+    // Disable CS
+    gpio_clear(LCD_CS);
 }
 
 int main(void) {
-    int i;
-    int polarity_toggle = 0;
 
-    clock_setup();
+    //clock_setup();
     gpio_setup();
     spi_setup();
 
-    // Indicate startup is complete
+    // Turn on the backlight.
     gpio_set(GPIOC, GPIO9);
-    for (i = 0; i < 2500000; i++) __asm__("nop");	/* Wait a bit. */
-    gpio_clear(GPIOC, GPIO9);
 
-    bool sent = false;
+    // Send the clear all command.
+    gpio_set(LCD_CS);
+    spi_send8(LCD_SPI, 0x20);
+    spi_send8(LCD_SPI, 0x00);
+    gpio_clear(LCD_CS);
 
-    for (;;) {
-        if (!gpio_get(GPIOA, GPIO0)) {
-            gpio_set(GPIOC, GPIO9);
+    uint8_t k = 0;
+    uint8_t colors[] = {0x00, 0x88, 0x44, 0x22, 0x66, 0xAA, 0xCC, 0xFF};
+    uint8_t color = 0;
 
-            if (!sent){
-                gpio_set(GPIOG, GPIO12);
-                spi_send(SPI3, 0x2004);
-                spi_send(SPI3, 0x0000);
-                gpio_clear(GPIOG, GPIO12);
+    while(true) {
 
-                sent = true;
-            }
+        send_line(k++, colors[color]);
 
-        } else {
-            gpio_clear(GPIOC, GPIO9);
-
-            if (sent){
-                gpio_set(GPIOG, GPIO12);
-                spi_send(SPI3, 0x2004 | 0x1428);
-                spi_send(SPI3, 0x0000);
-                gpio_clear(GPIOG, GPIO12);
-
-                sent = false;
-            }
+        if (k >= 176) {
+            k = 0;
+            if (++color > 7) color = 0;
         }
 
-        if (polarity_toggle++ > 1000000) {
-            // TOGGLE Display polarity
-            gpio_set(GPIOG, GPIO14);
-            for (i = 0; i < 1000; i++) __asm__("nop");	/* Wait a bit. */
-            gpio_clear(GPIOG, GPIO14);
-            polarity_toggle = 0;
-        }
+        // Periodically invert the polarity bit so the crystals don't degrade.
+        gpio_set(GPIOG, GPIO14);
+        for (int i = 0; i < 1000; i++) __asm__("nop");	/* Wait a bit. */
+        gpio_clear(GPIOG, GPIO14);
     }
 
     return 0;
